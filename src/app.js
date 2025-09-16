93fcc023086f295df4ffd2fb98c3a329b96bac62
nikita-konkin <niki-konkin@yandex.ru>
2025-09-16 11:58:30 +0300

    bugfix

--- src/app.js
+++ src/app.js
@@ -1,38 +1,37 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function greet(name) {
-    return `Hello, ${name}!`;
+  return `Hello, ${name}!`;
 }
 
 module.exports = { add, multiply, greet };
 
 if (require.main === module) {
-    console.log('Application started!');
-    console.log('2 + 3 =', add(2, 3));
-    console.log('4 * 5 =', multiply(4, 5));
-    console.log(greet('Student'));
+  console.log("Application started!");
+  console.log("2 + 3 =", add(2, 3));
+  console.log("4 * 5 =", multiply(4, 5));
+  console.log(greet("Student"));
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        throw new Error('Division by zero');
-    }
-    return a / b;
+  if (b === 0) {
+    throw new Error("Division by zero");
+  }
+  return a / b;
 }
 
 module.exports = { add, multiply, greet, subtract, divide, power };
-}
 
 function power(base, exponent) {
-    return Math.pow(base, exponent);
+  return Math.pow(base, exponent);
 }
